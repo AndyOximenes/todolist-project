b8fbfd7ca887d32ad9e0734f54e7867ee6f5bc6c
AndyOximenes <andy.oximenes@gmail.com>
2020-02-28 17:05:47 -0300

    Setting up date inside the code

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.get("/", (req, res)=> {
-    res.send("Hello world!")
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000.");
+app.get("/", (req, res) => {
+
+  var today = new Date()
+  var currentDay = today.getDay()
+
+  if(currentDay === 6 ||currentDay === 0) {
+      res.send("<h1>It's the weekend, brother! Take it easy ;)</h1>")
+  } else {
+      res.send("<h1>Let's work, dude!</h1>")
+  }
     
-})
+  
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
