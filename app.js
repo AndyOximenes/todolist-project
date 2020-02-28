96fc03051afcd6d11becd785d72c30549aa3bafb
AndyOximenes <andy.oximenes@gmail.com>
2020-02-28 18:36:28 -0300

    Add EJS template and SWITCH statement to return weekday

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-  var today = new Date()
-  var currentDay = today.getDay()
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
 
-  if(currentDay === 6 ||currentDay === 0) {
-      res.send("<h1>It's the weekend, brother! Take it easy ;)</h1>")
-  } else {
-      res.send("<h1>Let's work, dude!</h1>")
+    case 1:
+      day = "Monday";
+      break;
+
+    case 2:
+      day = "Tuesday";
+      break;
+
+    case 3:
+      day = "Wednesday";
+      break;
+
+    case 4:
+      day = "Thursday";
+      break;
+
+    case 5:
+      day = "Friday";
+      break;
+
+    case 6:
+      day = "Saturday";
+      break;
+    default:
   }
-    
-  
+
+  res.render("list", { kindOfDay: day });
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
