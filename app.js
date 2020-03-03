641d4304ad0d3e9be48af30d363beab64c000a0a
AndyOximenes <andy.oximenes@gmail.com>
2020-03-03 11:10:23 -0300

    Change var for Let

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
